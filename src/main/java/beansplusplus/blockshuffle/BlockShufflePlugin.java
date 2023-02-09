ef5ce15f3912f9c32039302206ea5cd7c0b981cb
Tomay0 <thomaspage33@gmail.com>
2023-02-09 20:14:37 +1300

    Write block shuffle rules

--- src/main/java/beansplusplus/blockshuffle/BlockShufflePlugin.java
+++ src/main/java/beansplusplus/blockshuffle/BlockShufflePlugin.java
@@ -4,6 +4,7 @@ import beansplusplus.beansgameplugin.BeansGamePlugin;
 import beansplusplus.beansgameplugin.GameConfiguration;
 import beansplusplus.beansgameplugin.GameCreator;
 import beansplusplus.beansgameplugin.GameState;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -35,7 +36,13 @@ public class BlockShufflePlugin extends JavaPlugin implements GameCreator {
 
   @Override
   public List<String> rulePages() {
-    return List.of("TODO");
+    return List.of(
+        "Block shuffle is a game where you race to find randomly generated blocks as fast as possible.",
+        "Each round, every person is given a randomly generated block. The first person to find the block gets a point for that round. First person to 10 blocks wins.",
+        "Default settings will make the round last forever until the block is found by someone. You can force rounds to end earlier if you configure the " + ChatColor.RED + "minutes_per_block" + ChatColor.BLACK + " setting.",
+        "By default, PVP and hunger is off and keep inventory is on. These settings can be changed in the config if you like.",
+        "Using the config, you can also alter the number of blocks to win, or turn off " + ChatColor.RED + "share_blocks" + ChatColor.BLACK + " such that every player gets a different block per round."
+    );
   }
 
   @Override
